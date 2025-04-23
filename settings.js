3847a093197a9bf22b38516bc47cad8d528606f2
Icedripper <lirugillan@gmail.com>
2025-04-23 14:00:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "KillerDrone",
+  botOwner: 'Ice Dripper', // Your name
+  ownerNumber: '254104751220', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
